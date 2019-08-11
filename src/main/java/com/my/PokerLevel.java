ca59b2621ff0a8f73d863d4ad4192174c216d1a7
zzwgo <kenzzw@163.com>
2019-08-11 20:42:30 +0800

    Add: Test for each player has three of a kind

--- src/main/java/com/my/PokerLevel.java
+++ src/main/java/com/my/PokerLevel.java
@@ -1,8 +1,8 @@
 package com.my;
 
 public enum PokerLevel {
-    HIGH_POINT(1), PAIR(2), TWO_PAIRS(3), THREE_OF_A_KIND(4), STRAIGHT(5),
-    FLUSH(6), FULL_HOUSE(7), FOUR_OF_A_KIND(8), STRAIGHT_FLUSH(9);
+    HIGH_POINT(1), PAIR(2), THREE_OF_A_KIND(3), STRAIGHT(4),
+    FLUSH(5), FULL_HOUSE(6), FOUR_OF_A_KIND(7), STRAIGHT_FLUSH(8);
     private int level;
 
     PokerLevel(int level) {
--- src/test/java/com/my/PockerHandsTest.java
+++ src/test/java/com/my/PockerHandsTest.java
@@ -124,6 +124,15 @@ public class PockerHandsTest {
         assertEquals(2,result);
     }
 
+    @Test
+    public void should_return_2_when_call_excute_given_each_player_five_poker_and_each_has_three_of_A_kind(){
+        List<Pocker> pockerListA= getPockerList(Arrays.asList("2H","2D","2S","5D","AC"));
+        List<Pocker> pockerListB= getPockerList(Arrays.asList("2S","2C","6S","6C","6D"));
+
+        int result=pockerHands.excute(pockerListA,pockerListB);
+
+        assertEquals(2,result);
+    }
     private List<Pocker> getPockerList(List<String> values){
         List<Pocker> pockerList= new ArrayList<Pocker>();
         for(String value:values){
