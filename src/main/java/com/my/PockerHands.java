c446a482cff43e68fd4e03952f49fb1ae0728ffc
zzwgo <kenzzw@163.com>
2019-08-11 16:56:18 +0800

    Add: Test for compare single poker

--- src/main/java/com/my/PockerHands.java
+++ src/main/java/com/my/PockerHands.java
@@ -1,7 +1,9 @@
 package com.my;
 
-public class PockerHands {
-    public void excute(){
+import java.util.List;
 
+public class PockerHands {
+    public int excute(List<Pocker> player1,List<Pocker> player2){
+        return player1.get(0).getPoint()>player2.get(0).getPoint()?1:2;
     }
 }
--- src/test/java/com/my/PockerHandsTest.java
+++ src/test/java/com/my/PockerHandsTest.java
@@ -1,10 +1,24 @@
 package com.my;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Collections;
+
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.*;
 
 public class PockerHandsTest {
 
+    private PockerHands pockerHands=new PockerHands();
 
+    @Test
+    public void should_return_2_when_call_excute_given_2H_and_3D(){
+        Pocker pockerA=new Pocker("2H");
+        Pocker pockerB=new Pocker("3D");
+
+        int result=pockerHands.excute(singletonList(pockerA),singletonList(pockerB));
+
+        assertEquals(2,result);
+    }
 }
